fa5c64829c5e13ed6140887f72f9d34ec05fb79e
IanPacini <ianpacini@hotmail.com>
2024-05-10 16:28:48 -0300

    Initial Commit

---
+++ Aula_Internacionalizacao/Aula_InternacionalizacaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aula_InternacionalizacaoApp.swift
+//  Aula_Internacionalizacao
+//
+//  Created by Ian Pacini on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Aula_InternacionalizacaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
