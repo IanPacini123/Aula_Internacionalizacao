8b41268862fca3410212fc6680bc5d00472c7808
IanPacini <ianpacini@hotmail.com>
2024-05-22 17:48:35 -0300

    Working Locale Manual method, commented code that should be explained in class

--- Aula_Internacionalizacao/LangSelectionView.swift
+++ Aula_Internacionalizacao/LangSelectionView.swift
@@ -1,18 +1,18 @@
 //
-//  ChangeLanguageView.swift
+//  LangSelectionView.swift
 //  Aula_Internacionalizacao
 //
-//  Created by Ian Pacini on 10/05/24.
+//  Created by Ian Pacini on 21/05/24.
 //
 
 import SwiftUI
 
-struct ChangeLanguageView: View {
+struct LangSelectionView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ChangeLanguageView()
+    LangSelectionView()
 }
